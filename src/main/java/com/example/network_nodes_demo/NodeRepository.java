d22617fcc5f22e38aadca956cd99d8b3fc9e61cb
Toni Dadic <toni.dadic@ericsson.com>
2026-05-22 15:10:01 +0200

    Rijeseno do tocke 19

---
+++ src/main/java/com/example/network_nodes_demo/NodeRepository.java
@@ -0,0 +1,28 @@
+package com.example.network_nodes_demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NodeRepository {
+
+    private final List<Node> nodes = new ArrayList<>();
+
+    public NodeRepository(){
+        nodes.add(new BscNode(1L, "BSC_ZG_01", "Ericsson", "Zagreb", 101));
+        nodes.add(new RncNode(2L, "RNC_ST_01", "Huawei", "Split", 24));
+        nodes.add(new BasebandNode(3L, "BB_RI_01", "Nokia", "Rijeka", "BB6630"));
+    }
+
+    public List<Node> findAll(){
+        return nodes;
+    }
+
+    public Node findById(Long id){
+        for (Node node : nodes){
+            if (node.getId().equals(id)){
+                return node;
+            }
+        }
+        return null;
+    }
+}
